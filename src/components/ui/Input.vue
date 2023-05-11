7642d4fbacd8c95d0e55ffd5027b50c18829b888
LegixCode <info@legix.ru>
2023-05-11 15:22:35 +0300

    1.23.1

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -18,21 +18,16 @@ const props = defineProps({
 });
 </script>
 <template>
-    <div
-        class="relative"
-    >
+    <div class="relative">
         <input
             :type="type"
             :placeholder="placeholder ?? ' '"
-            class="w-full pt-[10px] leading-4 h-9 text-sm focus:outline-none placeholder:opacity-0 focus:placeholder:opacity-100 border-b 
-            border-slate-300 focus:border-teal focus:shadow-[0_1px_0_0_#26a69a]"
+            class="w-full pt-[10px] leading-4 h-9 text-sm focus:outline-none placeholder:opacity-0 focus:placeholder:opacity-100 border-b border-slate-300 focus:border-teal focus:shadow-[0_1px_0_0_#26a69a]"
             :maxLength="maxLength"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)"
         />
-        <label
-            class="inline-block absolute top-0 left-0 text-sm pt-3 opacity-60 pointer-events-none origin-left"
-        >
+        <label class="inline-block absolute top-0 left-0 text-sm pt-3 opacity-60 pointer-events-none origin-left">
             {{ label }}
         </label>
     </div>
